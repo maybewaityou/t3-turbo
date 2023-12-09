6bb8d480d1c08ca89c8b1b15ca5bdcceb06685d6
MeePwn <maybewaityou@gmail.com>
2023-12-09 19:44:43 +0800

    first commit

---
+++ packages/tanstack/src/query.ts
@@ -0,0 +1,14 @@
+import { DefaultError, UseQueryResult } from "@tanstack/react-query";
+
+export function queryMatch<T, R>(
+  query: UseQueryResult<R, DefaultError>,
+  onLoading: () => T,
+  onFailure: (error: Error) => T,
+  onSuccess: (data: R) => T,
+): T {
+  const { isLoading, isError, error, data } = query;
+  if (isLoading) return onLoading();
+  if (isError) return onFailure(error);
+
+  return onSuccess(data as R);
+}
