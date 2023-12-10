ad38fb2c7f8beb2cdf85ac480824bedc638210d7
MeePwn <maybewaityou@gmail.com>
2023-12-11 05:24:07 +0800

    first commit

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -16,7 +16,7 @@ export default async function HomePage() {
   const hello = await api.post.hello.query({ text: "world" });
   const result = await kv.getObj<RouterOutputs["post"]["create"]>("post");
   return (
-    <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex h-full flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container mt-12 flex flex-col items-center justify-center gap-4 py-8">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Create <span className="text-pink-400">T3</span> Turbo
