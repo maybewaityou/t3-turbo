d4a6f3099c14b2a3c472c9229738416f01bb3743
MeePwn <maybewaityou@gmail.com>
2023-12-15 10:10:13 +0800

    first commit

--- packages/api/src/trpc.ts
+++ packages/api/src/trpc.ts
@@ -67,7 +67,15 @@ export const createTRPCContext = async (opts: {
   return createInnerTRPCContext({
     source,
     // session,
-    session: null,
+    session: {
+      user: {
+        id: "clpuj9f32000064mwwsgizh6m",
+        name: "MeePwn",
+        email: "maybewaityou@gmail.com",
+        image: "https://avatars.githubusercontent.com/u/8476488?v=4",
+      },
+      expires: "2099-12-31 23:59:59.000",
+    },
   });
 };
 
