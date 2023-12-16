c4cf18f249eff8397392a92c71ee10417a95bed6
MeePwn <maybewaityou@gmail.com>
2023-12-16 21:18:59 +0800

    first commit

---
+++ apps/elysia-server/src/index.ts
@@ -0,0 +1,30 @@
+/**
+ * Created by MeePwn
+ * https://github.com/maybewaityou
+ *
+ * description:
+ *
+ */
+
+import { cookie } from '@elysiajs/cookie'
+import { cors } from '@elysiajs/cors'
+import { swagger } from '@elysiajs/swagger'
+import { trpc } from '@elysiajs/trpc'
+import { Elysia } from 'elysia'
+
+import { createContext } from '~/context'
+import { env } from '~/env.mjs'
+
+import { appRouter } from '@acme/api'
+
+new Elysia()
+  .use(cors())
+  .use(cookie())
+  .use(swagger())
+  .use(
+    trpc(appRouter, {
+      endpoint: '/api/trpc',
+      createContext,
+    }),
+  )
+  .listen(env.PORT)
