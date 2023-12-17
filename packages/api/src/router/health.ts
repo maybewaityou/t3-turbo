7582327f3a9b814f4702cb46ab0beabdacb4177d
MeePwn <maybewaityou@gmail.com>
2023-12-18 07:00:34 +0800

    first commit

--- packages/api/src/router/health.ts
+++ packages/api/src/router/health.ts
@@ -8,10 +8,10 @@
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const healthRouter = createTRPCRouter({
-  status: publicProcedure.query(() => {
-    status: "ok";
-  }),
-  version: publicProcedure.query(() => {
-    version: "1.0.0";
-  }),
+  status: publicProcedure.query(() => ({
+    status: "ok",
+  })),
+  version: publicProcedure.query(() => ({
+    version: "1.0.0",
+  })),
 });
