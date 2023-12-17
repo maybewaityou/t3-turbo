eaee07b3dd8e62449e3cd6315af947e31dbbadef
MeePwn <maybewaityou@gmail.com>
2023-12-17 17:12:55 +0800

    first commit

--- packages/elysia/src/index.ts
+++ packages/elysia/src/index.ts
@@ -5,11 +5,14 @@
  * description:
  *
  */
+import { etag } from "@bogeychan/elysia-etag";
 import { cookie } from "@elysiajs/cookie";
 import { cors } from "@elysiajs/cors";
 import { swagger } from "@elysiajs/swagger";
 import { trpc } from "@elysiajs/trpc";
 import { Elysia } from "elysia";
+import { helmet } from "elysia-helmet";
+import { httpErrorDecorator } from "elysia-http-error";
 
 import { appRouter } from "@acme/api";
 import { createTRPCContext } from "@acme/api/src/trpc";
@@ -22,7 +25,13 @@ export const trpcCreateContext = () => {
   });
 };
 
-const app = new Elysia().use(cors()).use(cookie()).use(swagger());
+const app = new Elysia()
+  .use(etag())
+  .use(cors())
+  .use(helmet())
+  .use(cookie())
+  .use(swagger())
+  .use(httpErrorDecorator);
 
 export const trpcAppWithContext = (contextCreator: typeof trpcCreateContext) =>
   app.use(
