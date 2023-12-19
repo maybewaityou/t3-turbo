e39f1cd28db2f80581380effed7737122f01fa5e
MeePwn <maybewaityou@gmail.com>
2023-12-19 21:21:49 +0800

    first commit

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -6,37 +6,27 @@
  *
  */
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
-import {
-  createPostInput,
-  deletePostInput,
-  helloInput,
-  postByIdInput,
-} from "./schema/post";
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure.input(helloInput).query(({ input }) => {
+  hello: publicProcedure.query(({ input }) => {
     return {
-      greeting: `Hello ${input.text}`,
+      greeting: `Hello ${input}`,
     };
   }),
   all: publicProcedure.query(({ ctx }) => {
     return ctx.db.post.findMany({ orderBy: { id: "desc" } });
   }),
 
-  byId: publicProcedure.input(postByIdInput).query(({ ctx, input }) => {
+  byId: publicProcedure.query(({ ctx, input }: any) => {
     return ctx.db.post.findFirst({ where: { id: input.id } });
   }),
 
-  create: protectedProcedure
-    .input(createPostInput)
-    .mutation(async ({ ctx, input }) => {
-      await ctx.kv.setObj("post", input);
-      return ctx.db.post.create({ data: input });
-    }),
+  create: protectedProcedure.mutation(async ({ ctx, input }: any) => {
+    await ctx.kv.setObj("post", input);
+    return ctx.db.post.create({ data: input });
+  }),
 
-  delete: protectedProcedure
-    .input(deletePostInput)
-    .mutation(({ ctx, input }) => {
-      return ctx.db.post.delete({ where: { id: input } });
-    }),
+  delete: protectedProcedure.mutation(({ ctx, input }: any) => {
+    return ctx.db.post.delete({ where: { id: input } });
+  }),
 });
