19c879011fe3b1de198cb7f6d3891858e80ec1b4
MeePwn <maybewaityou@gmail.com>
2023-12-24 16:27:39 +0800

    first commit

--- packages/api/src/middleware/logger.ts
+++ packages/api/src/middleware/logger.ts
@@ -5,23 +5,27 @@
  * description:
  *
  */
+import chalk from "chalk";
 
 export async function loggerHandler({ type, path, ctx, next }: any) {
   const start = Date.now();
 
   const startMeta = { path, type };
   console.log(
-    `\n✨ tRPC request from ${ctx.headers.get("x-trpc-source")} start:`,
+    chalk.blue(
+      `\n✨ tRPC request from ${ctx.headers.get("x-trpc-source")} start:`,
+    ),
     startMeta,
   );
+
   const result = await next();
 
   const durationMs = Date.now() - start;
   const endMeta = { ...startMeta, durationMs };
 
   result.ok
-    ? console.log("🎯 OK request end timing:", endMeta)
-    : console.error("🐛 Non-OK request end timing", endMeta);
+    ? console.log(chalk.blue(`🎯 OK request end timing:`), endMeta)
+    : console.error(`🐛 Non-OK request end timing`, endMeta);
 
   return result;
 }
