76f3031af26495d0751e1a4886a6e75b15b13ec2
MeePwn <maybewaityou@gmail.com>
2024-01-08 06:36:54 +0800

    first commit

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -1,12 +1,24 @@
 // Importing env files here to validate on build
-import "@acme/auth/env.mjs";
-import "./src/env.mjs";
+import "@acme/api/env";
+import "@acme/auth/env";
+import "@acme/mqtt/env";
+import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@acme/api", "@acme/auth", "@acme/db"],
+  transpilePackages: [
+    "@acme/api",
+    "@acme/auth",
+    "@acme/cache",
+    "@acme/db",
+    "@acme/elysia",
+    "@acme/extensions",
+    "@acme/jwt",
+    "@acme/mqtt",
+    "@acme/tanstack",
+  ],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
--- apps/nextjs/src/env.js
+++ apps/nextjs/src/env.js
@@ -19,25 +19,9 @@ export const env = createEnv({
     USE_SERVER: z.string(),
     SERVER_HOST: z.string(),
     SERVER_PORT: z.string(),
-    JWT_SECRET: z.string().min(1),
     DATABASE_URL: z.string(),
     REDIS_KEY_PREFIX: z.string(),
     REDIS_URL: z.string(),
-    GITHUB_CLIENT_ID: z.string().min(1),
-    GITHUB_CLIENT_SECRET: z.string().min(1),
-    GOOGLE_CLIENT_ID: z.string().min(1),
-    GOOGLE_CLIENT_SECRET: z.string().min(1),
-    NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string().min(1)
-        : z.string().min(1).optional(),
-    NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url(),
-    ),
   },
   /**
    * Specify your client-side environment variables schema here.
@@ -56,17 +40,9 @@ export const env = createEnv({
     USE_SERVER: process.env.USE_SERVER,
     SERVER_HOST: process.env.SERVER_HOST,
     SERVER_PORT: process.env.SERVER_PORT,
-    JWT_SECRET: process.env.JWT_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
     REDIS_KEY_PREFIX: process.env.REDIS_KEY_PREFIX,
     REDIS_URL: process.env.REDIS_URL,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
   skipValidation:
     !!process.env.CI ||
