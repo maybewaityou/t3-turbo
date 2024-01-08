62ad335fb1d278afac67e5c13d0a691fdfaa1115
MeePwn <maybewaityou@gmail.com>
2024-01-09 07:38:42 +0800

    first commit

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -14,6 +14,12 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
     description: 'Generate a new package for the Acme Monorepo',
     prompts: [
       {
+        type: 'list',
+        name: 'type',
+        message: 'What type of package would you like to create?',
+        choices: ['vendors', 'packages'],
+      },
+      {
         type: 'input',
         name: 'name',
         message: 'What is the name of the package? (You can skip the `@acme/` prefix)',
@@ -35,27 +41,27 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
       },
       {
         type: 'add',
-        path: 'packages/{{ name }}/package.json',
+        path: '{{ type }}/{{ name }}/package.json',
         templateFile: 'templates/package.json.hbs',
       },
       {
         type: 'add',
-        path: 'packages/{{ name }}/tsconfig.json',
+        path: '{{ type }}/{{ name }}/tsconfig.json',
         templateFile: 'templates/tsconfig.json.hbs',
       },
       {
         type: 'add',
-        path: 'packages/{{ name }}/src/index.ts',
+        path: '{{ type }}/{{ name }}/src/index.ts',
         template: "export const name = '{{ name }}';",
       },
       {
         type: 'add',
-        path: 'packages/{{ name }}/__test__/index.spec.ts',
+        path: '{{ type }}/{{ name }}/__test__/index.spec.ts',
         template: testTmpl,
       },
       {
         type: 'modify',
-        path: 'packages/{{ name }}/package.json',
+        path: '{{ type }}/{{ name }}/package.json',
         async transform(content, answers) {
           if ('deps' in answers && typeof answers.deps === 'string') {
             const pkg = JSON.parse(content) as PackageJson
@@ -80,7 +86,7 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
           //   stdio: "inherit",
           // });
           execSync('pnpm i', { stdio: 'inherit' })
-          execSync(`pnpm prettier --write packages/${answers.name}/** --list-different`)
+          execSync(`pnpm prettier --write ${answers['type']}/${answers.name}/** --list-different`)
           return 'Package scaffolded'
         }
         return 'Package not scaffolded'
